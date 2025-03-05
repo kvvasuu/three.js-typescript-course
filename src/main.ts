383069b246c8324781c5dc22b3d29f9f8b1074ef
kvvasuu <lukaszkwas96@gmail.com>
2025-03-05 11:30:08 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./style.css";
+/* import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import Stats from "three/addons/libs/stats.module.js";
@@ -76,7 +76,7 @@ statsDiv?.appendChild(stats.dom);
 
 const gui = new GUI();
 
-/* scene.add(torus); */
+//scene.add(torus);
 
 const animation = {
   toggleAnimation: (): void => {
@@ -159,8 +159,19 @@ function animate() {
 
 animate();
 
+
+ */
+
+import "./style.css";
+import * as THREE from "three";
+import Stats from "three/addons/libs/stats.module.js";
+import { GUI } from "dat.gui";
+//import gsap from "gsap";
+
 const hideButton = document.getElementById("hide-ui");
 let isUIVisible = true;
+
+const statsDiv = document.getElementById("stats");
 
 const toggleUI = () => {
   if (isUIVisible) {
@@ -177,3 +188,102 @@ const toggleUI = () => {
 };
 
 hideButton?.addEventListener("click", toggleUI);
+
+const scene = new THREE.Scene();
+scene.add(new THREE.GridHelper());
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 2, 3);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshNormalMaterial();
+
+const cube = new THREE.Mesh(geometry, material);
+cube.position.y = 0.5;
+cube.position.x = 1;
+scene.add(cube);
+
+const torus = new THREE.Mesh(new THREE.TorusGeometry(0.5, 0.2), material);
+torus.position.y = 0.5;
+torus.position.x = -1;
+scene.add(torus);
+
+camera.lookAt(cube.position);
+
+const stats = new Stats();
+statsDiv?.appendChild(stats.dom);
+
+const gui = new GUI();
+
+let targetQuaternion = new THREE.Quaternion();
+
+const cameraActions = {
+  cameraLook: true,
+  toggleCamera: function () {
+    let targetPosition = this.cameraLook
+      ? torus.position.clone()
+      : cube.position.clone();
+
+    let lookAtMatrix = new THREE.Matrix4();
+    lookAtMatrix.lookAt(camera.position, targetPosition, camera.up);
+
+    let targetRotation = new THREE.Quaternion();
+    targetRotation.setFromRotationMatrix(lookAtMatrix);
+
+    targetQuaternion.copy(targetRotation);
+
+    this.cameraLook = !this.cameraLook;
+  },
+};
+
+document.addEventListener("keydown", (ev) => {
+  if (ev.key === " ") {
+    cameraActions.toggleCamera();
+  }
+});
+
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "x", -10, 10);
+cameraFolder.add(camera.position, "y", -10, 10);
+cameraFolder.add(camera.position, "z", -10, 10);
+cameraFolder.add(camera, "fov", 0, 180, 0.01).onChange(() => {
+  camera.updateProjectionMatrix();
+});
+cameraFolder.add(camera, "aspect", 0.00001, 10).onChange(() => {
+  camera.updateProjectionMatrix();
+});
+cameraFolder.add(camera, "near", 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix();
+});
+cameraFolder.add(camera, "far", 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix();
+});
+cameraFolder.add(cameraActions, "toggleCamera");
+cameraFolder.open();
+
+cameraActions.toggleCamera();
+function animate() {
+  requestAnimationFrame(animate);
+
+  camera.quaternion.slerp(targetQuaternion, 0.05);
+  renderer.render(scene, camera);
+
+  stats.update();
+}
+
+animate();
