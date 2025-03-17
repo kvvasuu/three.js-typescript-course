cd5d5b90922318310b9319b3bfac9135d70e951f
kvvasuu <lukaszkwas96@gmail.com>
2025-03-17 12:20:31 +0100

    .

--- src/course.ts
+++ src/course.ts
@@ -1,149 +1,42 @@
 import "./style.css";
-import {
-  Mesh,
-  Color,
-  MeshStandardMaterial,
-  BufferGeometry,
-  Raycaster,
-  Scene,
-  SpotLight,
-  PerspectiveCamera,
-  WebGLRenderer,
-  VSMShadowMap,
-  BoxGeometry,
-  CylinderGeometry,
-  TetrahedronGeometry,
-  PlaneGeometry,
-  Vector2,
-  Clock,
-  EquirectangularReflectionMapping,
-  MeshPhongMaterial,
-  Vector3,
-  MathUtils,
-} from "three";
+import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 import Stats from "three/addons/libs/stats.module.js";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+import RAPIER from "@dimforge/rapier3d-compat";
 
-function lerp(from: number, to: number, speed: number) {
-  const amount = (1 - speed) * from + speed * to;
-  return Math.abs(from - to) < 0.001 ? to : amount;
-}
+await RAPIER.init(); // This line is only needed if using the compat version
+const gravity = new RAPIER.Vector3(0.0, -9.81, 0.0);
+const world = new RAPIER.World(gravity);
+const dynamicBodies: [THREE.Object3D, RAPIER.RigidBody][] = [];
 
-class Pickable extends Mesh {
-  hovered = false;
-  clicked = false;
-  colorTo: Color;
-  defaultColor: Color;
-  geometry: BufferGeometry;
-  material: MeshStandardMaterial;
-  v = new Vector3();
+const scene = new THREE.Scene();
 
-  constructor(
-    geometry: BufferGeometry,
-    material: MeshStandardMaterial,
-    colorTo: Color
-  ) {
-    super();
-    this.geometry = geometry;
-    this.material = material;
-    this.colorTo = colorTo;
-    this.defaultColor = material.color.clone();
-    this.castShadow = true;
-  }
+const light1 = new THREE.SpotLight(undefined, Math.PI * 10);
+light1.position.set(2.5, 5, 5);
+light1.angle = Math.PI / 3;
+light1.penumbra = 0.5;
+light1.castShadow = true;
+light1.shadow.blurSamples = 10;
+light1.shadow.radius = 5;
+scene.add(light1);
 
-  update(delta: number) {
-    this.rotation.x += delta / 2;
-    this.rotation.y += delta / 2;
+const light2 = light1.clone();
+light2.position.set(-2.5, 5, 5);
+scene.add(light2);
 
-    // this.clicked
-    //   ? (this.position.y = MathUtils.lerp(this.position.y, 1, delta * 5))
-    //   : (this.position.y = MathUtils.lerp(this.position.y, 0, delta * 5));
-
-    //console.log(this.position.y)
-
-    // this.clicked
-    //   ? (this.position.y = lerp(this.position.y, 1, delta * 5))
-    //   : (this.position.y = lerp(this.position.y, 0, delta * 5));
-
-    this.hovered
-      ? this.material.color.lerp(this.colorTo, delta * 10)
-      : this.material.color.lerp(this.defaultColor, delta * 10);
-
-    this.hovered
-      ? (this.material.color.lerp(this.colorTo, delta * 10),
-        (this.material.roughness = lerp(
-          this.material.roughness,
-          0,
-          delta * 10
-        )),
-        (this.material.metalness = lerp(
-          this.material.metalness,
-          1,
-          delta * 10
-        )))
-      : (this.material.color.lerp(this.defaultColor, delta),
-        (this.material.roughness = lerp(this.material.roughness, 1, delta)),
-        (this.material.metalness = lerp(this.material.metalness, 0, delta)));
-
-    // this.clicked
-    //   ? this.scale.set(
-    //       MathUtils.lerp(this.scale.x, 1.5, delta * 5),
-    //       MathUtils.lerp(this.scale.y, 1.5, delta * 5),
-    //       MathUtils.lerp(this.scale.z, 1.5, delta * 5)
-    //     )
-    //   : this.scale.set(
-    //       MathUtils.lerp(this.scale.x, 1.0, delta),
-    //       MathUtils.lerp(this.scale.y, 1.0, delta),
-    //       MathUtils.lerp(this.scale.z, 1.0, delta)
-    //     );
-
-    this.clicked
-      ? this.scale.set(
-          lerp(this.scale.x, 1.5, delta * 5),
-          lerp(this.scale.y, 1.5, delta * 5),
-          lerp(this.scale.z, 1.5, delta * 5)
-        )
-      : this.scale.set(
-          lerp(this.scale.x, 1.0, delta),
-          lerp(this.scale.y, 1.0, delta),
-          lerp(this.scale.z, 1.0, delta)
-        );
-
-    this.clicked ? this.v.set(1.5, 1.5, 1.5) : this.v.set(1.0, 1.0, 1.0);
-    this.scale.lerp(this.v, delta * 5);
-  }
-}
-
-const scene = new Scene();
-
-const spotLight = new SpotLight(0xffffff, 500);
-spotLight.position.set(5, 5, 5);
-spotLight.angle = 0.3;
-spotLight.penumbra = 0.5;
-spotLight.castShadow = true;
-spotLight.shadow.radius = 20;
-spotLight.shadow.blurSamples = 20;
-spotLight.shadow.camera.far = 20;
-scene.add(spotLight);
-
-await new RGBELoader().loadAsync("img/venice_sunset_1k.hdr").then((texture) => {
-  texture.mapping = EquirectangularReflectionMapping;
-  scene.environment = texture;
-});
-
-const camera = new PerspectiveCamera(
+const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   100
 );
-camera.position.set(0, 2, 4);
+camera.position.set(0, 2, 5);
 
-const renderer = new WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = VSMShadowMap;
+renderer.shadowMap.type = THREE.VSMShadowMap;
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener("resize", () => {
@@ -154,14 +47,116 @@ window.addEventListener("resize", () => {
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-controls.maxPolarAngle = Math.PI / 2 + Math.PI / 16; // ~ 100 degrees
+controls.target.y = 1;
 
-const raycaster = new Raycaster();
-const pickables: Pickable[] = []; // used in the raycaster intersects methods
-let intersects;
-const mouse = new Vector2();
+// Cuboid Collider
+const cubeMesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshNormalMaterial()
+);
+cubeMesh.castShadow = true;
+scene.add(cubeMesh);
+const cubeBody = world.createRigidBody(
+  RAPIER.RigidBodyDesc.dynamic().setTranslation(0, 5, 0).setCanSleep(false)
+);
+const cubeShape = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5)
+  .setMass(1)
+  .setRestitution(1);
+world.createCollider(cubeShape, cubeBody);
+dynamicBodies.push([cubeMesh, cubeBody]);
 
-renderer.domElement.addEventListener("pointerdown", (e) => {
+// Ball Collider
+const sphereMesh = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshNormalMaterial()
+);
+sphereMesh.castShadow = true;
+scene.add(sphereMesh);
+const sphereBody = world.createRigidBody(
+  RAPIER.RigidBodyDesc.dynamic().setTranslation(-2, 5, 0).setCanSleep(false)
+);
+const sphereShape = RAPIER.ColliderDesc.ball(1).setMass(1).setRestitution(1.1);
+world.createCollider(sphereShape, sphereBody);
+dynamicBodies.push([sphereMesh, sphereBody]);
+
+// Cylinder Collider
+const cylinderMesh = new THREE.Mesh(
+  new THREE.CylinderGeometry(1, 1, 2, 16),
+  new THREE.MeshNormalMaterial()
+);
+cylinderMesh.castShadow = true;
+scene.add(cylinderMesh);
+const cylinderBody = world.createRigidBody(
+  RAPIER.RigidBodyDesc.dynamic().setTranslation(0, 5, 0).setCanSleep(false)
+);
+const cylinderShape = RAPIER.ColliderDesc.cylinder(1, 1)
+  .setMass(1)
+  .setRestitution(1.1);
+world.createCollider(cylinderShape, cylinderBody);
+dynamicBodies.push([cylinderMesh, cylinderBody]);
+
+// ConvexHull Collider
+const icosahedronMesh = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(1, 0),
+  new THREE.MeshNormalMaterial()
+);
+icosahedronMesh.castShadow = true;
+scene.add(icosahedronMesh);
+const icosahedronBody = world.createRigidBody(
+  RAPIER.RigidBodyDesc.dynamic().setTranslation(2, 5, 0).setCanSleep(false)
+);
+const points = new Float32Array(
+  icosahedronMesh.geometry.attributes.position.array
+);
+const icosahedronShape = (
+  RAPIER.ColliderDesc.convexHull(points) as RAPIER.ColliderDesc
+)
+  .setMass(1)
+  .setRestitution(1.1);
+world.createCollider(icosahedronShape, icosahedronBody);
+dynamicBodies.push([icosahedronMesh, icosahedronBody]);
+
+// Trimesh Collider
+const torusKnotMesh = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(),
+  new THREE.MeshNormalMaterial()
+);
+torusKnotMesh.castShadow = true;
+scene.add(torusKnotMesh);
+const torusKnotBody = world.createRigidBody(
+  RAPIER.RigidBodyDesc.dynamic().setTranslation(4, 5, 0)
+);
+const vertices = new Float32Array(
+  torusKnotMesh.geometry.attributes.position.array
+);
+let indices = new Uint32Array(
+  (torusKnotMesh.geometry.index as THREE.BufferAttribute).array
+);
+const torusKnotShape = (
+  RAPIER.ColliderDesc.trimesh(vertices, indices) as RAPIER.ColliderDesc
+)
+  .setMass(1)
+  .setRestitution(1.1);
+world.createCollider(torusKnotShape, torusKnotBody);
+dynamicBodies.push([torusKnotMesh, torusKnotBody]);
+
+const floorMesh = new THREE.Mesh(
+  new THREE.BoxGeometry(100, 1, 100),
+  new THREE.MeshPhongMaterial()
+);
+floorMesh.receiveShadow = true;
+floorMesh.position.y = -1;
+scene.add(floorMesh);
+const floorBody = world.createRigidBody(
+  RAPIER.RigidBodyDesc.fixed().setTranslation(0, -1, 0)
+);
+const floorShape = RAPIER.ColliderDesc.cuboid(50, 0.5, 50);
+world.createCollider(floorShape, floorBody);
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+renderer.domElement.addEventListener("click", (e) => {
   mouse.set(
     (e.clientX / renderer.domElement.clientWidth) * 2 - 1,
     -(e.clientY / renderer.domElement.clientHeight) * 2 + 1
@@ -169,82 +164,43 @@ renderer.domElement.addEventListener("pointerdown", (e) => {
 
   raycaster.setFromCamera(mouse, camera);
 
-  intersects = raycaster.intersectObjects(pickables, false);
-
-  // toggles `clicked` property for only the Pickable closest to the camera
-  intersects.length &&
-    ((intersects[0].object as Pickable).clicked = !(
-      intersects[0].object as Pickable
-    ).clicked);
-
-  // toggles `clicked` property for all overlapping Pickables detected by the raycaster at the same time
-  intersects.forEach((i) => {
-    (i.object as Pickable).clicked = !(i.object as Pickable).clicked;
-  });
-});
-
-renderer.domElement.addEventListener("mousemove", (e) => {
-  mouse.set(
-    (e.clientX / renderer.domElement.clientWidth) * 2 - 1,
-    -(e.clientY / renderer.domElement.clientHeight) * 2 + 1
+  const intersects = raycaster.intersectObjects(
+    [cubeMesh, sphereMesh, cylinderMesh, icosahedronMesh, torusKnotMesh],
+    false
   );
 
-  raycaster.setFromCamera(mouse, camera);
-
-  intersects = raycaster.intersectObjects(pickables, false);
-
-  pickables.forEach((p) => (p.hovered = false));
-
-  intersects.length && ((intersects[0].object as Pickable).hovered = true);
+  if (intersects.length) {
+    dynamicBodies.forEach((b) => {
+      b[0] === intersects[0].object &&
+        b[1].applyImpulse(new RAPIER.Vector3(0, 10, 0), true);
+    });
+  }
 });
-
-const cylinder = new Pickable(
-  new CylinderGeometry(0.66, 0.66),
-  new MeshStandardMaterial({ color: 0x888888 }),
-  new Color(0x008800)
-);
-scene.add(cylinder);
-pickables.push(cylinder);
-
-const cube = new Pickable(
-  new BoxGeometry(),
-  new MeshStandardMaterial({ color: 0x888888 }),
-  new Color(0xff2200)
-);
-cube.position.set(-2, 0, 0);
-scene.add(cube);
-pickables.push(cube);
-
-const pyramid = new Pickable(
-  new TetrahedronGeometry(),
-  new MeshStandardMaterial({ color: 0x888888 }),
-  new Color(0x0088ff)
-);
-pyramid.position.set(2, 0, 0);
-scene.add(pyramid);
-pickables.push(pyramid);
-
-const floor = new Mesh(new PlaneGeometry(20, 20), new MeshPhongMaterial());
-floor.rotateX(-Math.PI / 2);
-floor.position.y = -1.25;
-floor.receiveShadow = true;
-//floor.material.envMapIntensity = 0
-scene.add(floor);
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
-const clock = new Clock();
-let delta = 0;
+const gui = new GUI();
+
+const physicsFolder = gui.addFolder("Physics");
+physicsFolder.add(world.gravity, "x", -10.0, 10.0, 0.1);
+physicsFolder.add(world.gravity, "y", -10.0, 10.0, 0.1);
+physicsFolder.add(world.gravity, "z", -10.0, 10.0, 0.1);
+
+const clock = new THREE.Clock();
+let delta;
 
 function animate() {
   requestAnimationFrame(animate);
 
   delta = clock.getDelta();
+  world.timestep = Math.min(delta, 0.1);
+  world.step();
 
-  pickables.forEach((p) => {
-    p.update(delta);
-  });
+  for (let i = 0, n = dynamicBodies.length; i < n; i++) {
+    dynamicBodies[i][0].position.copy(dynamicBodies[i][1].translation());
+    dynamicBodies[i][0].quaternion.copy(dynamicBodies[i][1].rotation());
+  }
 
   controls.update();
 
